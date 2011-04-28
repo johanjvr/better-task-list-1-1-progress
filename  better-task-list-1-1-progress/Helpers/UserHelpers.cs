5a158cef854ec497454ed82cc33fddb8896cbbe5
geovanimartinez@yovasolutions.com <geovanimartinez@yovasolutions.com@8f3a2a06-9307-209d-8a93-5ebd939c852e>
2011-04-28 00:01:05 +0000

    Intial import

---
+++  better-task-list-1-1-progress/Helpers/UserHelpers.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Web;
+using System.Linq;
+using System.Web.Security;
+using BetterTaskList.Models;
+using System.Collections.Generic;
+
+namespace BetterTaskList.Helpers
+{
+    public class UserHelpers
+    {
+        static BetterTaskListDataContext db = new BetterTaskListDataContext();
+
+        public static Guid GetUserId(string userName)
+        {
+            return (from u in db.Users where u.UserName.Equals(userName) select u.UserId).SingleOrDefault();
+        }
+
+        /// <summary>
+        /// Returns a profile object given the UserId
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public static Profile GetUserProfile(Guid userId)
+        {
+            return (from u in db.Profiles where u.UserId.Equals(userId) select u).SingleOrDefault();
+        }
+
+        /// <summary>
+        /// Returns a profile object given the ProfileId
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static Profile GetUserProfile(long id)
+        {
+            return (from u in db.Profiles where u.ProfileId.Equals(id) select u).SingleOrDefault();
+        }
+
+        public static string GetUserEmailAddress(Guid userId)
+        {
+            // NOTE: we use the email address value as the username as well
+            return (from u in db.Users where u.UserId.Equals(userId) select u.LoweredUserName).SingleOrDefault();
+        }
+
+        public static string GetUserFullName(Guid userId)
+        {
+            return (from u in db.Profiles where u.UserId.Equals(userId) select u.FullName).SingleOrDefault();
+        }
+
+        public static string GetUserFullName(string userName)
+        {
+            Guid userId = GetUserId(userName);
+            return (from u in db.Profiles where u.UserId.Equals(userId) select u.FullName).SingleOrDefault();
+        }
+
+        public static string GetResetUserPassword(string userName)
+        {
+            MembershipUser mu = Membership.GetUser(userName);
+            if(mu !=null)
+            {
+                // unlock the user first then reset password. 
+                // otherwise you will get a runtime error.
+                mu.UnlockUser();
+                return mu.ResetPassword();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+    }
+}
---
+++  better-task-list-1-1-progress/Scripts/cufon-replace.js
@@ -0,0 +1,14 @@
+Cufon.replace('h2, h3, #menu ul li, #head-button', { fontFamily: 'Swis721 Cn BT', hover: 'true' });
+Cufon.replace('.link, .link-1, #contact-form a', { fontFamily: 'Swis721 Cn BT', textShadow: '#f6f6f6 1px 1px', hover: 'true' });
+
+Cufon.replace('.box-1 h2', { fontFamily: 'Swis721 Cn BT', textShadow: '#92cb1f 1px 1px' });
+Cufon.replace('.box-2 h2', { fontFamily: 'Swis721 Cn BT', textShadow: '#92cb1f 1px 1px' });
+Cufon.replace('.box-3 h2', { fontFamily: 'Swis721 Cn BT', textShadow: '#92cb1f 1px 1px' });
+
+Cufon.replace('.box-4 h2', { fontFamily: 'Swis721 Cn BT', textShadow: '#77b03e 1px 1px' });
+Cufon.replace('.box-5 h2', { fontFamily: 'Swis721 Cn BT', textShadow: '#77b03e 1px 1px' });
+Cufon.replace('.box-6 h2', { fontFamily: 'Swis721 Cn BT', textShadow: '#77b03e 1px 1px' });
+
+Cufon.replace('.box-7 h2', { fontFamily: 'Swis721 Cn BT', textShadow: '#5c971e 1px 1px' });
+Cufon.replace('.box-8 h2', { fontFamily: 'Swis721 Cn BT', textShadow: '#5c971e 1px 1px' });
+Cufon.replace('.box-9 h2', { fontFamily: 'Swis721 Cn BT', textShadow: '#5c971e 1px 1px' });
