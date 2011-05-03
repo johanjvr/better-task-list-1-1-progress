58f2adb74331c15834270096bcf2081407893fd8
geovanimartinez@yovasolutions.com <geovanimartinez@yovasolutions.com@8f3a2a06-9307-209d-8a93-5ebd939c852e>
2011-05-03 23:24:00 +0000

    After a ticket is created EditDraft is called rather then Edit. Edit will now be used for Updating rather then Submitting a draft Ticket. Both EditDraft and Edit now submit an ActivityFeed record.

---
+++ trunk/Models/Activity/ActivityFeed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetterTaskList.Models
+{
+    public partial class ActivityFeed
+    {
+        BetterTaskListDataContext db = new BetterTaskListDataContext();
+
+        public string FeedCreatorFullName
+        {
+            get { return (from r in db.Profiles where r.UserId.Equals(FeedActionCreatorUserId) select r.FirstName + " " + r.LastName).Single(); }
+        }
+
+    }
+}
