d1a8e3c70e6d4f3fdb97f226f53f9d2bddaab7a8
geovanimartinez@yovasolutions.com <geovanimartinez@yovasolutions.com@8f3a2a06-9307-209d-8a93-5ebd939c852e>
2011-05-04 18:53:19 +0000

    Ehhancements to the way comments are displayed ans submitted.

---
+++ trunk/Areas/Tickets/Models/TicketComment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetterTaskList.Models
+{
+    public partial class TicketComment
+    {
+        BetterTaskListDataContext db = new BetterTaskListDataContext();
+
+        public string CommentSubmitterFullName
+        {
+            get { return (from r in db.Profiles where r.UserId.Equals(TicketCommentSubmitterUserId) select r.FirstName + " " + r.LastName).Single(); }
+        }
+    }
+
+}
+
