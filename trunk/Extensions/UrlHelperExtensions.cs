8d82411fced898921bedeada17fdea42efc15db4
geovanimartinez@yovasolutions.com <geovanimartinez@yovasolutions.com@8f3a2a06-9307-209d-8a93-5ebd939c852e>
2011-05-10 21:56:09 +0000

    Various UI enhancements.

---
+++ trunk/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace BetterTaskList.Extensions
+{
+    public static class UrlHelpers
+    {
+        //TODO: Question the existance of this class (could do with Url.Content(""))
+        public static string AccountPicture(this UrlHelper helper, string name, string size)
+        {
+            if (string.IsNullOrEmpty(name))
+                name = "default";
+
+            return helper.Content(string.Format("~/Content/Avatars/Pictures/{0}_{1}.png", name, size));
+        }
+    }
+}
--- trunk/Models/Activity/ActivityFeed.cs
+++ trunk/Models/Activity/ActivityFeed.cs
@@ -14,6 +14,62 @@ namespace BetterTaskList.Models
             get { return (from r in db.Profiles where r.UserId.Equals(FeedActionCreatorUserId) select r.FirstName + " " + r.LastName).Single(); }
         }
 
+        public string FeedCreator128x128AvatarUrl
+        {
+            get
+            {
+                string pictureName = (from r in db.Profiles where r.UserId.Equals(FeedActionCreatorUserId) select r.PictureName).Single();
+                if (string.IsNullOrEmpty(pictureName))
+                {
+                    return string.Format("~/Content/Avatars/{0}_128x128.png", "Default");
+                }
+
+                return string.Format("~/Content/Avatars/Pictures/{0}_128x128.png", pictureName);
+            }
+        }
+
+        public string FeedCreator64x64AvatarUrl
+        {
+            get
+            {
+                string pictureName = (from r in db.Profiles where r.UserId.Equals(FeedActionCreatorUserId) select r.PictureName).Single();
+                if (string.IsNullOrEmpty(pictureName))
+                {
+                    return string.Format("~/Content/Avatars/{0}_64x64.png", "Default");
+                }
+
+                return string.Format("~/Content/Avatars/Pictures/{0}_64x64.png", pictureName);
+            }
+        }
+
+        public string FeedCreator32x32AvatarUrl
+        {
+            get
+            {
+                string pictureName = (from r in db.Profiles where r.UserId.Equals(FeedActionCreatorUserId) select r.PictureName).Single();
+                if (string.IsNullOrEmpty(pictureName))
+                {
+                    return string.Format("~/Content/Avatars/{0}_32x32.png", "Default");
+                }
+
+                return string.Format("~/Content/Avatars/Pictures/{0}_32x32.png", pictureName);
+            }
+        }
+
+        public string FeedCreator16x16AvatarUrl
+        {
+            get
+            {
+                string pictureName = (from r in db.Profiles where r.UserId.Equals(FeedActionCreatorUserId) select r.PictureName).Single();
+                if (string.IsNullOrEmpty(pictureName))
+                {
+                    return string.Format("~/Content/Avatars/{0}_16x16.png", "Default");
+                }
+
+                return string.Format("~/Content/Avatars/Pictures/{0}_16x16.png", pictureName);
+            }
+        }
+
         public string FeedPostedTimeFrame
         {
 
@@ -29,17 +85,17 @@ namespace BetterTaskList.Models
                 //// if you dont take in to account the amount of hours you may end up 
                 //// with something like Today @ 4:30PM for something that was posted
                 //// yesterday afternoon asuming a total of 24hrs have not passed.
-                
-                if(days == 0 && hours < 1)
+
+                if (days == 0 && hours < 1)
                 {
-                    if(min < 1)
+                    if (min < 1)
                     {
                         return sec + " seconds ago";
                     }
 
-                    return  min + " minutes ago";
+                    return min + " minutes ago";
                 }
-                
+
                 if (days == 0 && hours < 12)
                 {
                     return "Today @ " + FeedActionTimeStamp.ToLocalTime().ToShortTimeString();
