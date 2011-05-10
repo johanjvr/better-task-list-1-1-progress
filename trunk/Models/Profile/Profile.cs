e61626a2bd9acbfa5cfe8be715f799baf3872335
geovanimartinez@yovasolutions.com <geovanimartinez@yovasolutions.com@8f3a2a06-9307-209d-8a93-5ebd939c852e>
2011-05-10 13:44:11 +0000

    UrlHelperExtensions no longer required. put the picture url code at the Profile.cs class level. works well.

---
+++ trunk/Models/Profile/Profile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetterTaskList.Models
+{
+    public partial class Profile
+    {
+        public string Picture128x128Url
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(PictureName))
+                {
+                    return string.Format("~/Content/Avatars/{0}_128x128.png", "Default");
+                }
+
+                return string.Format("~/Content/Avatars/Pictures/{0}_128x128.png", PictureName);
+            }
+        }
+
+
+    }
+}
