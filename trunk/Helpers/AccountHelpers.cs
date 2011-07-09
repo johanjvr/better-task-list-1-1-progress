7753ef81200934da10f2193b88a85489cdb1a579
geovanimartinez@yovasolutions.com <geovanimartinez@yovasolutions.com@8f3a2a06-9307-209d-8a93-5ebd939c852e>
2011-07-09 08:13:56 +0000

    Initial implementation of the Mobile Service & TimeZones selectList on the MyAccount.cshtml page.

---
+++ trunk/Helpers/AccountHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace BetterTaskList.Helpers
+{
+    public class AccountHelpers
+    {
+
+
+    }
+}
